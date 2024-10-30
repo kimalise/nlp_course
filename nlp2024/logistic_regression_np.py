73ec2ca5cbc19a24a9f90251bb66598be1bff4d3
kimalise <jinguozhe@ybu.edu.cn>
2024-10-30 16:44:48 +0800

    20241030

--- nlp2024/logistic_regression_np.py
+++ nlp2024/logistic_regression_np.py
@@ -1,6 +1,12 @@
+from tokenize import generate_tokens
+
 import numpy as np
 import matplotlib.pyplot as plt
 from torch import dtype
+
+num_epochs = 100
+learning_rate = 0.01
+batch_size = 4
 
 # 生成数据
 def generate_data():
@@ -40,4 +46,99 @@ def split_data(x1, x2, y, train_rate=0.8):
 
     return train_x1, train_x2, train_y, val_x1, val_x2, val_y
 
+class DataLoader():
+    def __init__(self, x1, x2, y, batch_size):
+        self.x1 = x1
+        self.x2 = x2
+        self.y = y
+        self.batch_size = batch_size
 
+    def get_batch(self, batch_index):
+        start = batch_index * self.batch_size
+        end = min(len(self.x1), (batch_index + 1) * self.batch_size)
+        return self.x1[start:end], self.x2[start:end], self.y[start:end]
+
+# 数据集
+def test_dataloader():
+    x1, x2, y = generate_data()
+    data = split_data(x1, x2, y, train_rate=0.8)
+    train_x1, train_x2, train_y, val_x1, val_x2, val_y = data
+    batch_size = 4
+    dataloader = DataLoader(train_x1, train_x2, train_y, batch_size)
+    num_batch = len(train_x1) // batch_size
+    for i in range(num_batch):
+        batch = dataloader.get_batch(i)
+        print(batch)
+
+# 逻辑回归模型
+class LogisticRegressionModel():
+    def __init__(self):
+        self.w1 = 0
+        self.w2 = 0
+        self.b = 0
+        self.grad_w1 = 0
+        self.grad_w2 = 0
+        self.grad_b = 0
+
+    def sigmoid(self, z):
+        return 1 / (1 + np.exp(-z))
+
+    def forward(self, x1, x2):
+        z = self.w1 * x1 + self.w2 * x2 + self.b
+        return self.sigmoid(z)
+
+    def loss(self, x1, x2, y):
+        l = y * np.log(self.forward(x1, x2)) + (1 - y) * np.log(1 - self.forward(x1, x2))
+        return -np.mean(l)
+
+    def backward(self, x1, x2, y):
+        self.grad_w1 = np.sum((self.forward(x1, x2) - y) * x1)
+        self.grad_w2 = np.sum((self.forward(x1, x2) - y) * x2)
+        self.grad_b = np.sum((self.forward(x1, x2) - y))
+
+    def step(self, lr):
+        self.w1 = self.w1 - lr * self.grad_w1
+        self.w2 = self.w2 - lr * self.grad_w2
+        self.b = self.b - lr * self.grad_b
+
+def train(model, dataloader):
+    num_batch = len(dataloader.x1) // batch_size
+    for epoch in range(num_epochs):
+        for step in range(num_batch):
+            batch = dataloader.get_batch(step)
+            batch_x1, batch_x2, batch_y = batch
+            pred_y = model.forward(batch_x1, batch_x2)
+            loss = model.loss(batch_x1, batch_x2, batch_y)
+
+            model.backward(batch_x1, batch_x2, batch_y)
+            model.step(learning_rate)
+
+            print("Epoch: {}, Step: {}, Loss: {:.4f}".format(epoch, step, loss))
+# w1 * x1 + w2 * x2 + b = 0
+# x2 = -(w1 / w2) * x1 - (b / w2)
+def boundary_line(model, x):
+    return -(model.w1 / model.w2) * x - (model.b / model.w2)
+
+if __name__ == '__main__':
+    # test_dataloader()
+    x1, x2, y = generate_data()
+    train_x1, train_x2, train_y, val_x1, val_x2, val_y = split_data(x1, x2, y)
+    train_dataloader = DataLoader(train_x1, train_x2, train_y, batch_size)
+    val_dataloader = DataLoader(val_x1, val_x2, val_y, batch_size)
+
+    model = LogisticRegressionModel()
+
+    train(model, train_dataloader)
+
+    plt.scatter(x1, x2, c=y)
+
+    line_x1 = []
+    line_x2 = []
+    for i in np.arange(0, 6, 0.1):
+        j = boundary_line(model, i)
+        line_x1.append(i)
+        line_x2.append(j)
+    plt.plot(line_x1, line_x2)
+    plt.show()
+
+    print("finish")
