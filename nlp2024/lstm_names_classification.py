818db3be1c013b3d6d22daa8a7a0cea5721fcd64
kimalise <jinguozhe@ybu.edu.cn>
2024-12-18 17:12:30 +0800

    lstm name classification

--- nlp2024/lstm_names_classification.py
+++ nlp2024/lstm_names_classification.py
@@ -5,6 +5,16 @@ import unicodedata
 import string
 import os
 from torch.utils.data import Dataset, DataLoader
+from torch.nn.utils.rnn import pad_sequence, pack_padded_sequence
+from sklearn.metrics import f1_score
+
+from nlp2024.linear_torch import optimizer
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+epochs = 10
+learning_rate = 1e-3
+batch_size = 128
 
 allowed_characters = string.ascii_letters + " .,;'"
 n_letters = len(allowed_characters)
@@ -58,13 +68,141 @@ def split_dataset(names, labels, train_rate):
 class NameDataset(Dataset):
     def __init__(self, names, labels, idx2char, char2idx, idx2label, label2idx):
         super(NameDataset, self).__init__()
+        self.names = names
+        self.labels = labels
+        self.idx2char = idx2char
+        self.char2idx = char2idx
+        self.idx2label = idx2label
+        self.label2idx = label2idx
+        self.name_lens = [len(name) for name in self.names]
+        sort_index = np.argsort(self.name_lens)
+        sort_index = sort_index[::-1]
+        self.names = np.array(self.names)[sort_index].tolist()
+        self.labels = np.array(self.labels)[sort_index].tolist()
+        self.name_lens = np.array(self.name_lens)[sort_index].tolist()
+
+        self.names = [[self.char2idx[c] for c in name] for name in self.names]
+        self.labels = [self.label2idx[l] for l in labels]
 
     def __len__(self):
-        pass
+        return len(self.names)
 
     def __getitem__(self, index):
-        pass
+        return self.names[index], self.labels[index], self.name_lens[index]
+
+def collate_fn(batch):
+    '''
+        batch: [(name, label, name_len), (), ()]
+        name: [B, s] [s, B]
+        label: [B]
+        name_len: [B]
+    '''
+    names = [torch.tensor(item[0]) for item in batch]
+    labels = torch.tensor([item[1] for item in batch])
+    name_lens = torch.tensor([item[2] for item in batch])
+
+    names = pad_sequence(names, batch_first=False, padding_value=0)
+
+    return names, labels, name_lens
+
+class LSTMModel(torch.nn.Module):
+    def __init__(self, vocab_size, char_vec_dim, hidden_dim, output_dim):
+        super(LSTMModel, self).__init__()
+        self.embedding = torch.nn.Embedding(
+            vocab_size,
+            char_vec_dim
+        )
+
+        self.lstm = torch.nn.LSTM(
+            input_size=char_vec_dim,
+            hidden_size=hidden_dim,
+            bidirectional=True,
+            num_layers=1,
+            batch_first=False,
+        )
+
+        self.fc = torch.nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, names, name_lens):
+        '''
+        :param names: [s, B]
+        :param name_lens: [B]
+        :return:
+        '''
+        names_emb = self.embedding(names) # [s, B, d]
+        names_packed = pack_padded_sequence(
+            names_emb,
+            name_lens,
+            batch_first=False
+        )
+        output, (h, c) = self.lstm(names_packed)
+        # [2, B, d] h[0] + h[1]
+        fc_output = self.fc(h[0] + h[1]) # [B, 18]
+        # fc_output = self.fc(torch.cat([h[0], h[1]], dim=1))
+        return fc_output
+
+def test_dataset():
+    names, labels, idx2char, char2idx, idx2label, label2idx = preprocess()
+    dataset = NameDataset(names, labels, idx2char, char2idx, idx2label, label2idx)
+    dataloader = DataLoader(
+        dataset,
+        batch_size=4,
+        shuffle=False,
+        collate_fn=collate_fn
+    )
+    for step, batch in enumerate(dataloader):
+        print(batch)
+
+def train(model, train_dataloader, val_dataloader):
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    model.to(device)
+
+    best_f1 = -1
+    for epoch in range(epochs):
+        model.train()
+        for step, batch in enumerate(train_dataloader):
+            names, labels, name_lens = batch
+            names = names.to(device)
+            labels = labels.to(device)
+            name_lens = name_lens.to(device)
+
+            pred_logits = model(names, name_lens)
+            loss = criterion(pred_logits, labels)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            print("Epoch: {}, Step: {}, Loss: {:.4f}".format(epoch, step, loss.item()))
+
+        f1 = test(model, val_dataloader)
+        print("Validation f1: {:.4f}".format(f1))
+        if f1 > best_f1:
+            torch.save(model.state_dict(), "best_model.pth")
+            best_f1 = f1
+
+def test(model, dataloader):
+    model.eval()
+
+    golden_labels = []
+    predicted_labels = []
+    with torch.no_grad():
+        for step, batch in enumerate(dataloader):
+            names, labels, name_lens = batch
+            names = names.to(device)
+            labels = labels.to(device)
+            name_lens = name_lens.to(device)
+
+            pred_logits = model(names, name_lens)
+            pred_labels = torch.argmax(pred_logits, dim=1)
+            golden_labels.extend(labels.cpu().numpy().tolist())
+            predicted_labels.extend(pred_labels.cpu().numpy().tolist())
+
+    f1 = f1_score(golden_labels, predicted_labels)
+    return f1
 
 if __name__ == '__main__':
-    names, labels, idx2char, char2idx, idx2label, label2idx = preprocess()
-    print("finish")
+    # names, labels, idx2char, char2idx, idx2label, label2idx = preprocess()
+    # print("finish")
+    test_dataset()
